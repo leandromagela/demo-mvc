0c1505b7784f3ae6462e1a9f15686e2e387339fc
Leandro Magela <leandromagela@gmail.com>
2018-05-27 12:42:30 -0300

    Conclusão Seção 4 - Camada de persistência
    - Configurando o Banco de Dados
    - Criando um DAO Genérico
    - Incluindo os DAO's específicos

---
+++ src/main/java/com/mballen/curso/boot/dao/DepartamentoDao.java
@@ -0,0 +1,18 @@
+package com.mballen.curso.boot.dao;
+
+import java.util.List;
+
+import com.mballen.curso.boot.domain.Departamento;
+
+public interface DepartamentoDao {
+	
+	void save(Departamento departamento);
+	
+	void update(Departamento departamento);
+	
+	void delete(Long id);
+	
+	Departamento findById(Long id);
+	
+	List<Departamento> findAll();
+}
